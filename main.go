236dcbb717fbdd16c741ac9e675b5738cc19711c
nathnael-demeke <codingwithnati@gmail.com>
2026-03-08 15:07:28 +0300

    Implemented ceasear encryption algorithm in go

--- main.go
+++ main.go
@@ -12,15 +12,29 @@ func hashText(key int, toBeHased string) {
 }
 
 func ceasarEncrypt(key int, text string) string {
+	//generate hasing text based on key provided
 	hashedText := strings.Split(originalLetters, "")
 	preHashes := hashedText[len(hashedText)-key:]
 	postHashes := hashedText[:len(hashedText)-key]
 	preHashes = append(preHashes, postHashes...)
-	fmt.Printf("%s \n", strings.Join(preHashes, ""))
-	return ""
+
+	encryptedText := ""
+
+	strings.Map(func(r rune) rune {
+		letterIndex := strings.Index(strings.ToLower(originalLetters), strings.ToLower(string(r)))
+
+		if letterIndex != -1 {
+			currentIndex := (letterIndex + len(text)) % len(text)
+			encryptedText += preHashes[currentIndex]
+		}
+
+		return r
+	}, text)
+
+	return encryptedText
 }
 func main() {
-	text := "Hello World!"
+	text := "Hello World"
 	encryptedText := ceasarEncrypt(5, text)
-	fmt.Printf("Encrypted Text %s", encryptedText)
+	fmt.Printf("%s", encryptedText)
 }

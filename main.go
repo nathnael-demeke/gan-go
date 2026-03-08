fcf2962c62942ca551a99c2c70e0dcb5140bb0c5
nathnael-demeke <codingwithnati@gmail.com>
2026-03-08 16:07:56 +0300

    trying decrypt

--- main.go
+++ main.go
@@ -7,25 +7,25 @@ import (
 
 const originalLetters string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-func hashText(key int, toBeHased string) {
-
-}
-
-func ceasarEncrypt(key int, text string) string {
-	//generate hasing text based on key provided
+func hashText(key int) []string {
 	hashedText := strings.Split(originalLetters, "")
 	preHashes := hashedText[len(hashedText)-key:]
 	postHashes := hashedText[:len(hashedText)-key]
 	preHashes = append(preHashes, postHashes...)
+	return preHashes
+}
+
+func ceasarEncrypt(key int, text string) string {
+	//generate hasing text based on key provided
 
 	encryptedText := ""
-
+	hashedText := hashText(key)
 	strings.Map(func(r rune) rune {
 		letterIndex := strings.Index(strings.ToLower(originalLetters), strings.ToLower(string(r)))
 
 		if letterIndex != -1 {
 			currentIndex := (letterIndex + len(text)) % len(text)
-			encryptedText += preHashes[currentIndex]
+			encryptedText += hashedText[currentIndex]
 		}
 
 		return r
@@ -33,8 +33,24 @@ func ceasarEncrypt(key int, text string) string {
 
 	return encryptedText
 }
+
+func ceasarDecrypt(key int, encryptedText string) string {
+	hasedText := hashText(key)
+	decryptedText := ""
+
+	strings.Map(func(letter rune) rune {
+		encryptedIndex := strings.Index(strings.Join(hasedText, ""), string([]rune{letter}))
+		encryptedIndex = (encryptedIndex + len(originalLetters)) % len(originalLetters)
+
+		decryptedText += string(originalLetters[encryptedIndex])
+		fmt.Printf("%d \n", encryptedIndex)
+		return 0
+	}, encryptedText)
+	return decryptedText
+}
+
 func main() {
 	text := "Hello World"
 	encryptedText := ceasarEncrypt(5, text)
-	fmt.Printf("%s", encryptedText)
+	fmt.Printf("%s %s", ceasarDecrypt(5, encryptedText), encryptedText)
 }
